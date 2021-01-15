71176ba5d454232a0c8c72b4d228cef6614cf85c
Ly Diem <lydieam@lys-macbook-pro.home>
2021-01-15 14:24:06 -0500

    initial push

---
+++ handler.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports.weather = async (event,context,callback) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        zipcode: "23421",
+        message: 'The weather in zip code' + zipcode + 'is 0, dress warm.',
+      }),
+  };
+  callback(null,response);
+};
+
+
